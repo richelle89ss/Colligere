f555a0d2a1dc9d4c64f0f7886235d0acb3d9767d
Richelle Tan <33138436+richelle89ss@users.noreply.github.com>
2017-10-27 12:27:57 +0800

    Update and rename countwords.java to Count Words.java

--- Java/Others/Count Words.java
+++ Java/Others/Count Words.java
@@ -3,8 +3,6 @@ import java.util.Scanner;
 /**
  * You enter a string into this program, and it will return how
  * many words were in that particular string
- * 
- * @author Marcus
  *
  */
   class CountTheWords{
@@ -21,6 +19,9 @@ import java.util.Scanner;
         public static int wordCount(String s){
           if(s.isEmpty() || s == null) return -1;
           return s.trim().split("[\\s]+").length;
+		
+		//End of code
+		
         }
         
   }
